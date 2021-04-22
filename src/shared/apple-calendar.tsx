aeeb937849d747beb93a7a7b267ae7f3bd3b2ece
Jose Reyes <gogims@gmail.com>
2021-04-22 00:18:17 -0500

    Programatically click the button

--- src/shared/apple-calendar.tsx
+++ src/shared/apple-calendar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import AppleIcon from '@material-ui/icons/Apple';
 import { Button, makeStyles } from "@material-ui/core";
+import axios from "axios";
 
 const useStyles = makeStyles(() => ({
     button: {
@@ -10,9 +11,20 @@ const useStyles = makeStyles(() => ({
 
 export const AppleCalendar: React.FC = () => {
     const classes = useStyles();
+    const handleClick = async () => {
+        const response = await axios.get('./calendar.ics');
+        const blob = new Blob([response.data], { type: 'text/calendar'});
+        const url = window.URL.createObjectURL(blob);
+
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = "calendar"
+        link.click();
+        window.URL.revokeObjectURL(url);
+    }
 
     return (
-        <Button rel="noreferrer" className={classes.button} href="./calendar.ics">
+        <Button className={classes.button} onClick={handleClick}>
             <AppleIcon /> &nbsp;Add To Apple Calendar
         </Button>
     );
