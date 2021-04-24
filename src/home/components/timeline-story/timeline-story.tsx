c826d7f6fd3c443a94d730d546de8aa15b80ae15
Jose Reyes <gogims@gmail.com>
2021-04-23 21:13:54 -0500

    Remove unused import

--- src/home/components/timeline-story/timeline-story.tsx
+++ src/home/components/timeline-story/timeline-story.tsx
@@ -5,7 +5,6 @@ import TimelineItem from '@material-ui/lab/TimelineItem';
 import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
 import TimelineConnector from '@material-ui/lab/TimelineConnector';
 import TimelineContent from '@material-ui/lab/TimelineContent';
-import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
 import TimelineDot from '@material-ui/lab/TimelineDot';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import FlightIcon from '@material-ui/icons/Flight';
